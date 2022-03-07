5b38d44450ae004a32741152cae8f83e5ccb1331
fancky <709737588@qq.com>
2022-03-07 21:01:01 +0800

    书签、mysql创建只读账号

--- SQL/MySql/mysql知识点.sql
+++ SQL/MySql/mysql知识点.sql
@@ -772,9 +772,47 @@ SHOW VARIABLES LIKE 'binlog_format'
 
 
 
+-- 创建账号
 
+SELECT  *  FROM mysql.user 
+ 
+  -- 删除用户
+ DROP USER 'masteraccount'@'%';
+ 
+-- 创建账户用于主从同步
+ CREATE USER 'masteraccount'@'%' IDENTIFIED BY '123456'; -- 创建用户
+ -- 修改登录方式  sqlyog无法登录
+ ALTER USER 'masteraccount'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+  
+-- 命令:GRANT privileges ON databasename.tablename TO 'username'@'host'
+  GRANT REPLICATION SLAVE ON *.* TO 'masteraccount'@'%' ;#分配主从权限 
+ 
+ 
+ GRANT ALL PRIVILEGES ON *.* TO 'masteraccount'@'%' ;-- 分配所有权限
+ 
+ GRANT ALL ON *.* TO 'masteraccount'@'%'; -- 分配所有权限
+ 
+  #root 为你数据库要连接用户的用户名，后面为密码
+ 
+ FLUSH PRIVILEGES;   #刷新权限
+ 
+ 
+ 
+ 
+ 
+ -- 创建只读账号
+ 
+ # 创建用户canalMysql
+create user canalMysql identified by '123456';
+# 给canal1授权访问
+GRANT SELECT, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'canalMysql'@'%';
+# 刷新权限
+flush privileges;
 
-
+-- 测试只能查询，不能删除
+select  *  from demo.demo_product;
+delete from demo.demo_product where id=2;
+ 
 
 
 
