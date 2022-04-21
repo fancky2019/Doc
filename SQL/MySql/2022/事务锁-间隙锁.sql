25d4dfcec42688acdee3564f94f5fe775861c0b8
fancky <709737588@qq.com>
2022-04-21 11:55:56 +0800

    netx-key lock锁范围：行锁+间隙锁

---
+++ SQL/MySql/2022/事务锁-间隙锁.sql
@@ -0,0 +1,91 @@
+-- record lock（记录锁，就是行锁）
+-- gap lock（间隙锁）
+-- next-key lock：record lock 和 gap lock
+
+-- innodb  默认锁  next-key lock
+
+
+-- innodb  默认锁  next-key lock
+--  gap  锁住区间   普通索引 条件具体值：值的上一个值和下一个值这段区间和相等的值（值允许重复）。如27的锁定区间[<27>]。InnoDB会给数据加上行锁和间隙锁
+--                           范围     ：锁定条件范围
+--                  群集索引：net_key lock降级为行锁，锁住当前行。行锁。
+--                  没有索引：锁表
+ 
+ 
+ 
+show index  from  person;
+
+create  index index_age on person (age);
+
+drop index  index_age on person; 
+
+select @@autocommit;
+
+
+show VARIABLES like '%autocommit%';
+
+-- 事务隔离级别
+show VARIABLES like '%iso%';
+
+
+-- 不自动提交事务。
+SET autocommit=0;
+
+
+-- 共享锁
+START TRANSACTION;
+UPDATE person SET name='ts12' WHERE id=15;
+commit;
+-- 
+-- age
+-- 
+-- 2
+-- 6
+-- 7
+-- 10
+-- 13
+-- 15
+-- 16
+-- 18
+-- 20
+-- 22
+-- 27
+-- 31
+
+
+ 
+-- 当前读
+START TRANSACTION;
+SELECT * FROM `person` WHERE age>22 for update;
+commit;					
+-- 26、27、31 锁住,没有索引将锁表
+START TRANSACTION;
+-- UPDATE person SET name='gap_lock_test1' WHERE age>22;
+UPDATE person SET name='gap_lock_test1' WHERE age=27;
+commit;
+
+--  快照度
+START TRANSACTION;
+SELECT * FROM `person` WHERE age>22;
+commit;
+
+
+
+delete  from person  where id>18;
+
+
+
+SELECT * FROM `person`；
+
+select  distinct age  from person;
+
+select  distinct age,count(*) from person
+GROUP BY age
+
+
+
+
+
+
+
+
---
+++ SQL/MySql/2022/事务锁-间隙锁_test.sql
@@ -0,0 +1,69 @@
+
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for person
+-- ----------------------------
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `age` int NULL DEFAULT NULL,
+  `birthday` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `index_age`(`age`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of person
+-- ----------------------------
+INSERT INTO `person` VALUES (1, 'fancky', 2, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+INSERT INTO `person` VALUES (2, 'fancky2', 7, '2022-03-08 19:54:29', '2022-03-02 10:36:54');
+INSERT INTO `person` VALUES (3, 'fancky china', 6, NULL, '2022-03-03 13:59:17');
+INSERT INTO `person` VALUES (4, 'fancky test', 10, NULL, '2022-03-10 14:15:50');
+INSERT INTO `person` VALUES (5, 'china fancky test', 15, NULL, '2022-03-09 16:10:18');
+INSERT INTO `person` VALUES (6, 'fancky read', 16, NULL, '2022-03-10 16:17:48');
+INSERT INTO `person` VALUES (7, 'you are very smart', 27, NULL, '2022-03-10 16:39:57');
+INSERT INTO `person` VALUES (9, 'shiwu', 20, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+INSERT INTO `person` VALUES (12, 'START TRANSACTIO2', 22, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+INSERT INTO `person` VALUES (14, 'shiwu1', 18, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+INSERT INTO `person` VALUES (15, 'ts12', 31, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+INSERT INTO `person` VALUES (16, 'shiwu_COMMIT', 13, '2022-03-09 19:54:11', '2022-03-01 10:36:48');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+
+
+
+
+SELECT * FROM `person` for update;
+
+
+INSERT INTO `person` (`name`,age,`birthday`,`update_time`) VALUES ( 'fancky', 21, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+INSERT INTO `person` (`name`,age,`birthday`,`update_time`) VALUES ( 'fancky', 22, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+INSERT INTO `person` (`name`,age,`birthday`,`update_time`) VALUES ( 'fancky', 26, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+INSERT INTO `person`  (`name`,age,`birthday`,`update_time`)VALUES( 'fancky', 27, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+
+INSERT INTO `person`  (`name`,age,`birthday`,`update_time`)VALUES( 'fancky', 28, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+
+INSERT INTO `person`  (`name`,age,`birthday`,`update_time`)VALUES( 'fancky', 31, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+INSERT INTO `person`  (`name`,age,`birthday`,`update_time`)VALUES( 'fancky', 32, '2022-04-13 19:54:11', '2022-04-19 10:36:48');
+
+
+
